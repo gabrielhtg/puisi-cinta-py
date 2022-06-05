fa9c1488da8399393060b719b1a7ad5c48686d7a
gabrielhtg <gabrielhutagalung970@gmail.com>
2022-06-05 20:59:19 +0700

    baru

---
+++ puisi-cinta.py
@@ -0,0 +1,22 @@
+from readline import read_init_file
+
+
+import os
+os.system('clear')
+
+nama = input("Masukkan nama kamu    : ")
+os.system('clear')
+
+print("Teruntuk kamu, " + nama)
+
+print("\nAku ingin mencintaimu dengan sederhana.")
+print("Dengan kata yang tak sempat diucapkan kayu,")
+print("kepada api yang menjadikannya abu.\n")
+
+print("Lagi dan lagi, aku ingin mengatakan bahwa,")
+print("Aku ingin mencintaimu dengan sederhana.")
+print("Dengan isyarat yang tak sempat disampaikan awan,")
+print("kepada hujan yang menjadikannya tiada.\n")
+
+print("Terima kasih telah hadir dan telah membaca.")
+print("Dari --> Gabriel Hutagalung <--")
